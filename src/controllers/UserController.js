1f7eabe8ce71cdb120e61a34ed1e3cdbdae89166
Luiz Pereira <leolima.ef@gmail.com>
2023-05-25 21:36:22 +0100

    Atualizando o banco

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 
 const sqliteconnection = require("../database/sqlite");
@@ -31,7 +31,7 @@ async create(request, response) {
 }
 
 async update(request, response) {
-  const { name, email } = request.body;
+  const { name, email, password, old_password } = request.body;
   const { id } = request.params;
 
   const database = await sqliteconnection();
@@ -47,17 +47,33 @@ async update(request, response) {
     throw new AppError("Este e-mail já está em uso.");
   }
 
-  user.name = name;
-  user.email = email;
+  user.name = name ?? user.name;
+  user.email = email ?? user.email;
+
+  if (password && !old_password) {
+    throw new AppError("Você precisa informar a senha antiga para definir uma nova");
+  }
+
+  if(password && old_password) {
+    const checkOldPassword = await compare(old_password, user.password);
+
+    if(!checkOldPassword) {
+      throw new AppError("A senha antiga não confere.");
+    }
+
+    user.password = await hash(password, 8);
+  }
+
 
   await database.run(
     `
     UPDATE users SET
     name = ?,
     email = ?,
-    updated_at = ?
+    password = ?,
+    updated_at = DATETIME('now')
     WHERE id = ?`,
-    [user.name, user.email, new Date(), id]
+    [user.name, user.email, user.password, id]
   );
 
   return response.status(201).json();
